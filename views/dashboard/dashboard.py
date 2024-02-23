fe003ee6fbe1f30a5652868844a15138def922a5
Kateto19519 <19519@uktc-bg.com>
2024-02-23 22:30:15 +0200

    1234admin_panel

--- views/dashboard/dashboard.py
+++ views/dashboard/dashboard.py
@@ -1,11 +1,9 @@
-
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 
-Builder.load_file('views/home_admin/home_admin.kv')
-class HomeAdmin(BoxLayout):
+
+Builder.load_file('views/dashboard/dashboard.kv')
+class Dashboard(BoxLayout):
     def __init__(self, **kw) -> None:
         super().__init__(**kw)
-
-
 
--- views/mi_add/menu_add.py
+++
@@ -1,9 +0,0 @@
-from kivy.lang import Builder
-from kivymd.uix.boxlayout import MDBoxLayout
-from kivymd.uix.screen import MDScreen
-
-Builder.load_file('views/mi_add/menu_add.kv')
-class MenuAdd(MDScreen):
-    def __init__(self, **kw):
-        super().__init__(**kw)
-
